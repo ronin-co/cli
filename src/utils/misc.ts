56ed1632a6a61f8e777189a5d5fe7431b6619bd7
Leo Lamprecht <leo@ronin.co>
2024-12-20 11:08:46 +0100

    Removed model formatting

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import type { parseArgs } from 'node:util';
 import { fieldsToCreate, fieldsToDrop } from '@/src/utils/field';
-import type { Model } from '@ronin/compiler';
+import type { Model, Result } from '@ronin/compiler';
 
 /** Represents a data item for logging */
 interface DataItem {
@@ -269,4 +269,65 @@ export const areArraysEqual = (arr1: Array<string>, arr2: Array<string>): boolea
   return arr1.every((obj, index) => {
     return JSON.stringify(obj) === JSON.stringify(arr2[index]);
   });
+};
+
+export type QueryResponse<T> = {
+  results: Array<Result<T>>;
+  error?: Error;
+};
+
+interface InvalidResponseErrorDetails {
+  message: string;
+  code: string;
+}
+
+export class InvalidResponseError extends Error {
+  message: InvalidResponseErrorDetails['message'];
+  code: InvalidResponseErrorDetails['code'];
+
+  constructor(details: InvalidResponseErrorDetails) {
+    super(details.message);
+
+    this.name = 'InvalidResponseError';
+    this.message = details.message;
+    this.code = details.code;
+  }
+}
+
+/**
+ * Parses the response as JSON or, alternatively, throws an error containing
+ * potential error details that might have been included in the response.
+ *
+ * @param response The response of a fetch request.
+ *
+ * @returns The response body as a JSON object.
+ */
+export const getResponseBody = async <T>(
+  response: Response,
+  options?: { errorPrefix?: string },
+): Promise<T> => {
+  // If the response is okay, we want to parse the JSON asynchronously.
+  if (response.ok) return response.json() as T;
+
+  const text = await response.text();
+
+  let json: T & {
+    error?: InvalidResponseErrorDetails;
+  };
+
+  try {
+    json = JSON.parse(text);
+  } catch (_err) {
+    throw new InvalidResponseError({
+      message: `${options?.errorPrefix ? `${options.errorPrefix} ` : ''}${text}`,
+      code: 'JSON_PARSE_ERROR',
+    });
+  }
+
+  if (json.error) {
+    json.error.message = `${options?.errorPrefix ? `${options.errorPrefix} ` : ''}${json.error.message}`;
+    throw new InvalidResponseError(json.error);
+  }
+
+  return json;
 };
