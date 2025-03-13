e00430e5e0491a9ed46ad71880af151eb88d3776
Leo Lamprecht <leo@ronin.co>
2025-03-13 10:22:42 +0100

    Removed unused code

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -11,9 +11,6 @@ import type * as CompilerPackage from '@ronin/compiler';
 import type * as SyntaxPackage from '@ronin/syntax/queries';
 import resolveFrom from 'resolve-from';
 
-/** List of not allowed model slugs. */
-const NOT_ALLOWED_MODEL_SLUGS = ['', 'model'];
-
 /** Represents a data item for logging */
 interface DataItem {
   slug?: string;
