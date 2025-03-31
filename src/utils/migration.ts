7721dfdc04723603fb7e1231949c9666c3a5bc8c
Ben <4991309+NuroDev@users.noreply.github.com>
2025-03-31 16:12:46 +0100

    Re-generate types after running `ronin apply` (#74)

--- src/utils/migration.ts
+++ src/utils/migration.ts
@@ -439,6 +439,7 @@ export const MIGRATION_FLAGS = {
   sql: { type: 'boolean', short: 's', default: false },
   local: { type: 'boolean', short: 'l', default: false },
   apply: { type: 'boolean', short: 'a', default: false },
+  'skip-types': { type: 'boolean', default: false },
 } satisfies NonNullable<Parameters<typeof parseArgs>[0]>['options'];
 
 /**
