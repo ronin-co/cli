5f7abe8a78ee239d58762bc8dbb6284c3c9d27e6
Cornelius <cornelius.denninger@gmail.com>
2025-04-16 10:18:30 +0200

    Added test for also dropping fields when updating meta

--- tests/utils/apply.test.ts
+++ tests/utils/apply.test.ts
@@ -519,6 +519,62 @@ describe('apply', () => {
           await db.query(transaction.statements);
           const rows = await getTableRows(db, models[0]);
 
+          expect(models[0].fields).toHaveLength(3);
+          expect(modelDiff).toHaveLength(6);
+          expect(models[0].slug).toBe('test');
+          expect(models[0].name).toBe('Test');
+          expect(models[0].pluralName).toBe('Tests');
+          expect(rows[0].id).toContain('corny_');
+          expect(rows[1].id).toContain('test_');
+        });
+
+        test('id prefix and drop fields', async () => {
+          const definedModel = model({
+            slug: 'test',
+            idPrefix: 'test',
+            fields: {
+              name: string(),
+            },
+          }) as unknown as Model;
+
+          const existingModel = model({
+            slug: 'test',
+            idPrefix: 'corny',
+            fields: {
+              name: string(),
+              age: number(),
+              email: string(),
+            },
+          }) as unknown as Model;
+
+          const insert = {
+            add: {
+              test: {
+                with: {
+                  name: 'Ilayda',
+                },
+              },
+            },
+          };
+
+          const transaction = new Transaction([insert], {
+            // @ts-expect-error This works once the types are fixed.
+            models: [definedModel, existingModel],
+            inlineParams: true,
+          });
+
+          const { db, models, modelDiff } = await runMigration(
+            // @ts-expect-error This works once the types are fixed.
+            [definedModel],
+            [existingModel],
+            {},
+            transaction.statements.map((statement) => statement),
+          );
+
+          await db.query(transaction.statements);
+          const rows = await getTableRows(db, models[0]);
+
+          expect(models[0].fields).toHaveLength(1);
           expect(modelDiff).toHaveLength(6);
           expect(models[0].slug).toBe('test');
           expect(models[0].name).toBe('Test');
